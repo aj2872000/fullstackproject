c6b34e81bbe3b8846ec72f7d8931a0e4b016be4e
ajay <aj2872000@gmail.com>
2023-01-22 17:28:31 +0530

    commit this change

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+
+
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -19,6 +21,10 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Email has alreaddy been registered");
   }
+
+  //encrypt password before saving to DB
+//   const salt = await bcrypt.genSalt(10);
+//   const hashedPassword = await bcrypt.hash(password, salt);
 
   //create new user if not present
   const user = await User.create({
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
@@ -20,7 +21,7 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, "please add a password"],
       minLength: [6, "Password must be up to 6 characters"],
-      maxLength: [23, "password must not be more than 23 characters"],
+      // maxLength: [23, "password must not be more than 23 characters"],
     },
     photo: {
       type: String,
@@ -41,6 +42,20 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+//Encrypt the password before saving to DB
+//here pre means before saving
+userSchema.pre("save", async function (next) {
+  //this is saying if password feild is not mofified during edit then go to next functiion
+  if (!this.isModified("password")) {
+    return next();
+  }
+  //Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
